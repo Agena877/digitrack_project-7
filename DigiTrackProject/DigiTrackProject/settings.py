2d1daf2c7115201d6598664e10207d685dd73fc1
Agena877 <22-42492@g.batstate-u.edu.ph>
2025-10-20 12:50:04 +0800

    Add STATIC_ROOT for Render deployment

--- DigiTrackProject/DigiTrackProject/settings.py
+++ DigiTrackProject/DigiTrackProject/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
                 os.path.join(BASE_DIR, 'tourism', 'static'), # Idagdag ito # type: ignore
             ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
